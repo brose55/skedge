b41aef70d198f4d29fa3d95c52da4a2861c538fa
Brandon Rose <brandonrose805@gmail.com>
2022-09-20 18:53:09 -0700

    delete session on signout

--- src/components/Header/AuthSwitcher.jsx
+++ src/components/Header/AuthSwitcher.jsx
@@ -1,10 +1,27 @@
+import axios from "axios"
 import { useContext } from "react"
 import AuthContext from '../../contexts/AuthContext'
 
 const AuthSwitcher = () => {
-  const {isSignedIn, setSignedIn } = useContext(AuthContext)
+  const { isSignedIn, setSignedIn } = useContext(AuthContext)
+  
+  const signOut = async () => {
+    try {
+      axios.delete(`${process.env.REACT_APP_DEV_URL}/api/sessions`, {withCredentials: true})
+    } catch (err) {
+      // TODO: change error handling here
+      console.error(err)
+    }
+  }
+
+  const handleClick = () => {
+    signOut()
+    setSignedIn(isSignedIn ? false : true)
+  }
+
+
   return (
-    <button onClick={() => {setSignedIn(isSignedIn ? false : true)}}>
+    <button onClick={handleClick}>
       {
         isSignedIn ? 'sign out' : 'sign in'
       }
