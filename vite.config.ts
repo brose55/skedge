f587f7a71c06936e63daa7cc56d626466883cdb0
Brandon Rose <brandonrose805@gmail.com>
2024-08-29 17:22:04 -0700

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: "dist",
-	},
-	base: "/skedge/",
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [react()],
+		build: {
+			outDir: "dist",
+		},
+		base: mode === "production" ? "/skedge/" : "/",
+	}
 })
